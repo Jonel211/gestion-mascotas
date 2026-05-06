ea08f6a0c63fecae7b83a8b7c3e1dca2e608263a
Jonel Aron <jonel.villanueva@tecsup.edu.pe>
2026-05-06 11:58:33 -0500

    Lin para subir imagenes a render

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for gestion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings # Importación correcta
 from django.conf.urls.static import static
-from gestion import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mascotas.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Solo añadimos esto una vez, con la condición de que estemos en local o en Render
+if settings.DEBUG or os.environ.get('RENDER'):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
